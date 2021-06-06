f87fa680b7a4362b7da57984aa9e9be5a64d24fc
Dzony12 <68508047+Dzony12@users.noreply.github.com>
2021-06-06 14:39:59 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,26 +9,6 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
 
-    /*---FILLING DATABASE WITH BOILERPLATE POSTS----*/
-    // if (!(await Post.countDocuments().exec()) > 0) {
-    //   let postsBoilerplate = [];
-    //   for (let i = 1; i <= 25; i++) {
-    //     postsBoilerplate.push({
-    //       text: `Post number ${i}, lorem ipsum jebać pis`,
-    //       author: "cycu_kiepski",
-    //       authorId: "5fe8873fcc4b5606258e0833",
-    //       createdAt: new Date(),
-    //     });
-    //   }
-
-    //   Promise.all([
-    //     postsBoilerplate.forEach(async (post) => await Post.create(post)),
-    //   ]).then(() =>
-    //     console.log(
-    //       "Added a few posts to the database for the collection to not be empty"
-    //     )
-    //   );
-    // }
     console.log(
       `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
     );
